14da2c659f8e0b581e0d6f0ccf8c4a90dbe4c174
Vittor Valler <vittorvc@gmail.com>
2020-06-10 14:29:06 -0300

    Fixed typo

--- projeto_django_alura_receitas/urls.py
+++ projeto_django_alura_receitas/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('', include('receitas.urls')),
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
