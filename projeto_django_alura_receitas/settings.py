199195d681f5e611366c0650889e9ca3dd205d87
Vittor Valler <vittorvc@gmail.com>
2020-06-15 13:14:42 -0300

    Debug

--- projeto_django_alura_receitas/settings.py
+++ projeto_django_alura_receitas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['appreceitas.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['appreceitas.herokuapp.com']
 
 
 # Application definition
