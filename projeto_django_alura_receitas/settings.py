2a359330ab051a90e863d43a30aa3fbe3fa057bc
Vittor Valler <vittorvc@gmail.com>
2020-06-19 10:48:38 -0300

    Debug

--- projeto_django_alura_receitas/settings.py
+++ projeto_django_alura_receitas/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['appreceitas.herokuapp.com']
+ALLOWED_HOSTS = ['appreceitas.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -139,5 +139,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 django_heroku.settings(locals())
