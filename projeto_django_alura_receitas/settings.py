1e4477852fca5aa7526192cc1a8ce38a47dccf93
Vittor Valler <vittorvc@gmail.com>
2020-07-28 16:58:36 -0300

    Implementing caching

--- projeto_django_alura_receitas/settings.py
+++ projeto_django_alura_receitas/settings.py
@@ -91,6 +91,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': os.environ.get('REDIS_URL'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
